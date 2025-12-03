b7bded78e57ecac89e5efcf447bd36b28369a05a
SIMONA-ANDREA COSMA <simona.cosma@stud.ubbcluj.ro>
2025-12-03 13:27:02 +0200

    Lab 9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -21,4 +21,27 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList) this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+    }
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        var product = listView.SelectedItem as Product;
+        if (product != null)
+        {
+            var shopl = (ShopList)this.BindingContext;
+            await App.Database.DeleteListProductAsync(shopl.ID, product.ID);
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 }
